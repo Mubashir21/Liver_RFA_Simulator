4e45896b51d0831174877b8a6736c7893813c0db
Mubashir21 <mubashirshoukat@gmail.com>
2024-04-25 12:38:17 +0800

    made changes to the model page. Added description and added a comparison section. added a loader animation.

--- front-end/src/components/PredictionForm.jsx
+++ front-end/src/components/PredictionForm.jsx
@@ -1,7 +1,51 @@
 import React, { useState, useRef, Fragment } from "react";
 import VideoPlayer from "./VideoPlayer";
-import { Dialog, Transition } from "@headlessui/react";
 import { IconX } from "@tabler/icons-react";
+import loader_icon from "../assets/loader_icon.webp";
+
+// VideoModal Component
+function VideoModal({ isOpen, onClose, videoPath }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div className="relative bg-white rounded-lg p-4">
+        <button onClick={onClose} className="absolute top-0 right-0 p-4">
+          <IconX size={24} />
+        </button>
+        <h3 className="text-3xl font-bold leading-6 text-gray-900 text-center pt-2 ">
+          Prediction Successful
+        </h3>
+        <div className="mt-2">
+          <VideoPlayer
+            videoPath={videoPath}
+            className="aspect-video shadow-2xl border-solid border border-black rounded-xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// LoadingModal Component
+function LoadingModal({ isLoading, onClose }) {
+  if (!isLoading) return null;
+
+  return (
+    <div className="fixed inset-0 z-10 overflow-y-auto">
+      <div
+        className="fixed inset-0 bg-black bg-opacity-25"
+        onClick={onClose}
+        aria-hidden="true"
+      ></div>
+      <div className="flex min-h-full items-center justify-center p-4 text-center">
+        <div className="relative bg-white rounded-2xl p-6 text-left align-middle shadow-xl">
+          <img src={loader_icon} alt="loading icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function PredictionForm() {
   const [file, setFile] = useState(null);
@@ -50,160 +94,79 @@ function PredictionForm() {
       }
     } catch (error) {
       console.error("Error:", error);
+      alert("An error occurred while predicting. Please try again."); // Show error feedback
+    } finally {
+      setIsLoading(false); // Ensure loading is set to false in all cases
+      fileInputRef.current.value = ""; // Reset the file input
     }
-
-    setIsLoading(false);
     if (fileInputRef.current) {
       fileInputRef.current.value = "";
     }
   };
 
   return (
-    <div className="h-96 flex items-center justify-center">
-
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col space-y-4 p-4 rounded-lg"
+    <div className="border border-solid rounded-lg border-gray-300 py-5 shadow-2xl px-8">
+      <p className="mx-auto font-bold text-center text-3xl mb-6">
+        Simulation Form
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col space-y-4 rounded-lg"
+      >
+        <label htmlFor="file" className="px-4 -mb-4">
+          Initial liver condition
+        </label>
+        <input
+          type="file"
+          name="file"
+          accept=".npy"
+          onChange={handleFileChange}
+          ref={fileInputRef}
+          className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none
+      file:bg-gray-50 file:border-0 file:me-4 file:py-2 file:px-4"
+        />
+        <input
+          type="number"
+          placeholder="Enter k value"
+          value={k}
+          onChange={(e) => setK(e.target.value)}
+          className="py-2 px-4 focus:outline-none focus:border-blue-500"
+        />
+        <input
+          type="number"
+          placeholder="Enter w value"
+          value={w}
+          onChange={(e) => setW(e.target.value)}
+          className="py-2 px-4 focus:outline-none focus:border-blue-500"
+        />
+        <input
+          type="number"
+          placeholder="Enter sig value"
+          value={sig}
+          onChange={(e) => setSig(e.target.value)}
+          className="py-2 px-4 focus:outline-none focus:border-blue-500"
+        />
+        <input
+          type="number"
+          placeholder="Enter duration"
+          value={duration}
+          onChange={(e) => setDuration(e.target.value)}
+          className="py-2 px-4 focus:outline-none focus:border-blue-500"
+        />
+        <button
+          type="submit"
+          className="py-2 px-4 bg-black text-white rounded-md hover:bg-gray-800 focus:outline-none focus:bg-blue-600"
         >
-          <input
-            type="file"
-            accept=".npy"
-            onChange={handleFileChange}
-            ref={fileInputRef}
-            className="py-2 px-4 focus:outline-none focus:border-blue-500"
-          />
-          <input
-            type="number"
-            placeholder="Enter k value"
-            value={k}
-            onChange={(e) => setK(e.target.value)}
-            className="py-2 px-4 focus:outline-none focus:border-blue-500"
-          />
-          <input
-            type="number"
-            placeholder="Enter w value"
-            value={w}
-            onChange={(e) => setW(e.target.value)}
-            className="py-2 px-4 focus:outline-none focus:border-blue-500"
-          />
-          <input
-            type="number"
-            placeholder="Enter sig value"
-            value={sig}
-            onChange={(e) => setSig(e.target.value)}
-            className="py-2 px-4 focus:outline-none focus:border-blue-500"
-          />
-          <input
-            type="number"
-            placeholder="Enter duration"
-            value={duration}
-            onChange={(e) => setDuration(e.target.value)}
-            className="py-2 px-4 focus:outline-none focus:border-blue-500"
-          />
-          <button
-            type="submit"
-            className="py-2 px-4 bg-black text-white rounded-md hover:bg-gray-800 focus:outline-none focus:bg-blue-600"
-          >
-            Submit
-          </button>
-        </form>
-        {isLoading && (
-          <Transition appear show={isLoading} as={Fragment}>
-            <Dialog
-              as="div"
-              className="relative z-10"
-              onClose={() => setIsLoading(false)}
-            >
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="fixed inset-0 bg-black/25" />
-              </Transition.Child>
-
-              <div className="fixed inset-0 overflow-y-auto">
-                <div className="flex min-h-full items-center justify-center p-4 text-center">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                  >
-                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-lg font-medium leading-6 text-gray-900"
-                      >
-                        Loading...
-                      </Dialog.Title>
-                    </Dialog.Panel>
-                  </Transition.Child>
-                </div>
-              </div>
-            </Dialog>
-          </Transition>
-        )}
-        {videoPath && showPopup && (
-          <div>
-            <Transition appear show={showPopup} as={Fragment}>
-              <Dialog
-                as="div"
-                className="relative z-10"
-                onClose={() => setShowPopup(false)}
-              >
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="fixed inset-0 bg-black/25" />
-                </Transition.Child>
-
-                <div className="fixed inset-0 overflow-y-auto">
-                  <div className="flex min-h-full items-center justify-center p-4 text-center">
-                    <Transition.Child
-                      as={Fragment}
-                      enter="ease-out duration-300"
-                      enterFrom="opacity-0 scale-95"
-                      enterTo="opacity-100 scale-100"
-                      leave="ease-in duration-200"
-                      leaveFrom="opacity-100 scale-100"
-                      leaveTo="opacity-0 scale-95"
-                    >
-                      <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                        <span onClick={() => setShowPopup(false)}>
-                          <IconX />
-                        </span>
-                        <Dialog.Title
-                          as="h3"
-                          className="text-lg font-medium leading-6 text-gray-900"
-                        >
-                          Prediction successful.
-                        </Dialog.Title>
-                        <div className="mt-2">
-                          <VideoPlayer videoPath={videoPath} />
-                        </div>
-                      </Dialog.Panel>
-                    </Transition.Child>
-                  </div>
-                </div>
-              </Dialog>
-            </Transition>
-          </div>
-        )}
-      </div>
+          Predict
+        </button>
+      </form>
+      <LoadingModal isLoading={isLoading} onClose={() => setIsLoading(false)} />
+      <VideoModal
+        isOpen={showPopup}
+        onClose={() => setShowPopup(false)}
+        videoPath={videoPath}
+      />
+    </div>
   );
 }
 
--- front-end/src/pages/Model.jsx
+++ front-end/src/pages/Model.jsx
@@ -1,27 +1,163 @@
+import { useState } from "react";
+
 import PredictionForm from "../components/PredictionForm";
+import realPred from "../assets/best_prediction_3_1_1.mp4";
+import realPredRMSE from "../assets/3way_comparison.mp4";
 
 function Model() {
-    return (
-        <div className="relative h-screen">
-            <video
-                autoPlay
-                loop
-                muted
-                className="absolute inset-0 object-cover w-full h-full"
-                src="aboutbg.mp4"
-                type="video/mp4"
-            />
-            <div className="absolute inset-0 flex items-center justify-center">
-                <div className="absolute inset-0 bg-black opacity-50"></div>
-                <div className="sm:w-4/5 md:w-3/5 lg:w-2/3 xl:w-1/2 mx-auto max-w-xl">
-                    <div className="bg-white p-4 sm:p-8 rounded-lg shadow-md text-black relative z-10">
-                        <p className="text-center text-base sm:text-lg font-bold">Simulation Model Predict</p>
-                        <PredictionForm />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+  const [isError, setIsError] = useState(false);
+
+  return (
+    <div className="bg-slate-50 text-black">
+      <ModelPrediction />
+      <ModelExample isError={isError} setIsError={setIsError} />
+    </div>
+  );
 }
+
+const ModelPrediction = () => {
+  return (
+    <div className=" text-black h-full bg-slate-50 px-80">
+      <div className="flex justify-between py-16">
+        <div className="flex-col w-1/2">
+          <p className="text-black font-bold text-6xl mb-5">
+            Make Your Simulations A Reality
+          </p>
+          <p className="text-black text-2xl mb-5 my-2">
+            Model takes 5 parameters:
+          </p>
+          <ul className="list-disc pl-6">
+            <li>Inititial Liver Condition</li>
+            <li>Thermal Conductivity</li>
+            <li>Blood Perfusion Rate</li>
+            <li>Electrical Conductivity</li>
+            <li>Duration of Simulation</li>
+          </ul>
+          <p className="text-black my-5 text-justify">
+            To understand the model’s predictive capabilities, it’s essential to
+            consider the five key inputs it processes. First is the initial
+            liver condition, often visualized as a heatmap, which provides a
+            starting reference for the simulation, detailing the pre-ablation
+            temperature distribution and any existing anomalies within the liver
+            tissue. <br /> <br />
+            The second input, thermal conductivity, accounts for the liver’s
+            capacity to transfer heat, which affects how temperature gradients
+            evolve during ablation. Closely related is the third factor, blood
+            perfusion rate, which reflects the cooling effect provided by blood
+            flow – a critical component in predicting the extent of heat
+            distribution. The fourth input, electrical conductivity, is integral
+            as it determines how the radiofrequency energy is distributed
+            through the liver tissue, crucial for estimating the ablation zone.
+            Lastly, the duration of the simulation defines the timeframe over
+            which the ablation is modeled, dictating the temporal precision of
+            the procedure’s outcome predictions. Together, these inputs allow
+            the model to simulate the RFA process with remarkable detail,
+            providing clinicians with a predictive glimpse into the procedure's
+            potential results.
+          </p>
+        </div>
+        <div className="flex items-center">
+          <PredictionForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ModelExample = ({ isError, setIsError }) => {
+  return (
+    <div className="px-80 pb-16 bg-zinc-300">
+      <div className="py-8">
+        <div className=" bg-white z-30 w-fit flex justify-center m-auto p-3 border solid border-black rounded-lg">
+          <div
+            className={`border solid border-black  p-3 rounded-md mx-1 ${
+              isError ? "hover:bg-slate-400" : "bg-slate-500"
+            }`}
+            onClick={() => setIsError(false)}
+          >
+            Comparison
+          </div>
+          <div
+            className={`border solid border-black p-3 rounded-md mx-1 ${
+              isError ? "bg-slate-500" : "hover:bg-slate-400"
+            }`}
+            onClick={() => setIsError(true)}
+          >
+            Comparison with Error
+          </div>
+        </div>
+      </div>
+      {isError ? <PredTruthError /> : <TruthPred />}
+    </div>
+  );
+};
+
+const TruthPred = () => {
+  return (
+    <div className="flex">
+      <div className="flex-1">
+        <video
+          src={realPred}
+          controls
+          className="aspect-video shadow-2xl border-solid border border-black rounded-xl"
+        ></video>
+      </div>
+      <div className="flex-1 my-1 pl-16">
+        <p className="text-3xl font-bold mb-3">Ground Truth vs Prediction</p>
+        <p>
+          The video provides a side-by-side comparison: the actual ground truth
+          thermal map is displayed on the left, while the model's prediction is
+          showcased on the right. Upon examination, the gradients and thermal
+          distributions exhibit a remarkable congruence, highlighting the
+          model’s precision. This compelling alignment goes beyond superficial
+          similarity; it indicates an in-depth understanding by the model of the
+          complex interplay between heat transfer and tissue properties. The
+          nuanced variations in temperature, captured consistently across both
+          videos, underscore the model's ability to grasp and replicate the
+          intricate thermal dynamics at play during the ablation procedure. Such
+          accurate learning of the underlying physics is crucial, as it can
+          potentially lead to improved predictive capabilities in real-time
+          clinical settings, ultimately aiding in the customization of treatment
+          for individual patients and enhancing therapeutic outcomes.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const PredTruthError = () => {
+  return (
+    <div className="flex">
+      <div className="flex-1">
+        <video
+          src={realPredRMSE}
+          controls
+          className="aspect-video shadow-2xl border-solid border border-black rounded-xl"
+        ></video>
+      </div>
+      <div className="flex-1 my-1 pl-16">
+        <p className="text-3xl font-bold mb-3">
+          Ground Truth vs Prediction vs Error
+        </p>
+        <p>
+          The video arranges three critical plots for comparative analysis. On
+          the left, we have the ground truth thermal map, capturing the actual
+          state of the liver during ablation. Centered is the model's
+          prediction, a computed image that estimates the thermal spread. On the
+          right, the difference plot starkly reveals the discrepancies between
+          the ground truth and the prediction. This triptych presentation is a
+          powerful tool for evaluation, allowing us to appreciate not just the
+          model's accuracy in replicating the thermal dynamics but also to
+          pinpoint the specific areas and degrees of variance. The alignment
+          between the ground truth and prediction plots confirms the model's
+          robust learning capabilities, while the difference plot serves as a
+          guide for targeted improvements, ultimately striving for a model that
+          can mirror the complex reality of patient-specific treatments with
+          even greater fidelity.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Model;
